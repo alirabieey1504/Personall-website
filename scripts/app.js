960b0719de88ea79755deef69c3540519024ba50
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-01 23:56:56 +0330

    Add files via upload

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,7 @@
-const navbtn =document.querySelector('.nav__btn')
+const nav__btn =document.querySelector('.nav__btn')
+const menu__open =document.querySelector('.menu')
 
-navbtn.addEventListener('click',function (){
+nav__btn.addEventListener('click',function (){
   this.classList.toggle('nav__btn--open')
+  menu__open.classList.toggle('menu-open')
 })
