b96c2dd38c6d9db34777cb1493cc28c176ce8c26
alirabieey <139227807+alirabieey1504@users.noreply.github.com>
2023-12-03 13:34:57 +0330

    Add files via upload
    add cover

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,8 @@
 const nav__btn =document.querySelector('.nav__btn')
 const menu__open =document.querySelector('.menu')
-
+const cover=document.querySelector('.cover')
 nav__btn.addEventListener('click',function (){
   this.classList.toggle('nav__btn--open')
   menu__open.classList.toggle('menu-open')
+  cover.classList.toggle('cover--show')
 })
